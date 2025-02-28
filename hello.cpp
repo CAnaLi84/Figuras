ca54fc218f5e31d4725455a25de32b2f3218ebde
anali <ana_li84@ciencias.unam.mx>
2025-02-27 20:53:53 -0600

    Modificación de función para hacer línea con dos puntos. Se agrega la función cuadrado

--- hello.cpp
+++ hello.cpp
@@ -17,20 +17,38 @@
     std::vector<uint8_t> pixels;
 
 //Función lerp
-double lerp(int p1,int p2, double t)
+Punto lerp(Punto p1,Punto p2, double t)
 {
-    return (1-t)*p1 + t*p2;
+    double x = static_cast<double>((1-t)*p1.getX() + t*p2.getX());
+    double y = static_cast<double>((1-t)*p1.getY() + t*p2.getY());
+    return Punto(x,y);
+}
+
+//Función para saber si un punto está dentro de las dimensiones de la imágen
+bool dentro(Punto p)
+{
+    bool estaDentro = false;
+    if(p.getX() >= 0 && p.getX() < width && p.getY() >= 0 && p.getY() < height){
+        estaDentro = true;
+    }
+    return estaDentro;
+}
+
+//Función que calcula la distancia entre dos puntos
+double distancia(Punto p1, Punto p2)
+{
+    double dist = sqrt(pow(p2.getX() - p1.getX(), 2) + pow(p2.getY() - p1.getY(), 2));
+    return dist;
 }
 
 //Función para pintar una línea
-void linea(int p1, int p2, uint32_t color)
+void linea(Punto p1, Punto p2, uint32_t color)
 {
-    for(double t = 0; t < 1; t += 0.0001){
-        int x = static_cast<int>(lerp(p1, p2, t));
-        int y = x*2;
+    for(double t = 0; t < 1; t += 0.01){
+        Punto p = lerp(p1, p2, t);
 
-        if(x >= 0 && x < width && y >= 0 && y < height){
-         double index = (y * width + x) * 4;
+        if(dentro(p)){
+         double index = (p.getY() * width + p.getX()) * 4;
     
          pixels[index + 0] = (color >> 24) & 0xFF;
          pixels[index + 1] = (color >> 16) & 0xFF;
@@ -41,25 +59,44 @@ void linea(int p1, int p2, uint32_t color)
 }
 
 //Función para pintar un círculo
-//void circulo(int cx, int cy, int radio, uint32_t color)
-//{
-//    for(int alfa = 0; alfa < 360; alfa++){
-//        int x = radio * cos(alfa);
-//        int y = radio * sin(alfa);
-//        int index = ((y+cy) * width + (x+cy)) * 4;
-//
-//        pixels[index + 0] = (color >> 24) & 0xFF;
-//        pixels[index + 1] = (color >> 16) & 0xFF;
-//        pixels[index + 2] = (color >> 8) & 0xFF;
-//        pixels[index + 3] = color & 0xFF;
-//    }
-//}
+void circulo(int cx, int cy, int radio, uint32_t color)
+{
+    for(int alfa = 0; alfa < 360; alfa++){
+        int x = radio * cos(alfa);
+        int y = radio * sin(alfa);
+        int index = ((y+cy) * width + (x+cy)) * 4;
 
-//Función para pintar un cuadrado
+        pixels[index + 0] = (color >> 24) & 0xFF;
+        pixels[index + 1] = (color >> 16) & 0xFF;
+        pixels[index + 2] = (color >> 8) & 0xFF;
+        pixels[index + 3] = color & 0xFF;
+    }
+}
+
+//Función para pintar un cuadrado dados dos puntos horizontales
+void cuadrado(Punto p1, Punto p2, uint32_t color)
+{
+    double dist = distancia(p1, p2);
+    if(p1.getY() == p2.getY()){
+        Punto p3(p1.getX(), p1.getY()+dist);
+        Punto p4(p2.getX(), p2.getY()+dist);
+
+        linea(p1, p2, color);
+        linea(p2, p4, color);
+        linea(p3, p4, color);
+        linea(p3, p1, color);
+    }else{
+        std::cout << "Igrese dos puntos horizontales" << std::endl;
+    }
+}
 
 //Función para pintar un triángulo
+void triangulo()
+{}
 
 //Función para pintar hexágono
+void hexagono()
+{}
 
 //Función para pintar un fractal
 
@@ -99,14 +136,18 @@ int main() {
     // Crear una textura y un sprite para mostrar la imagen
     sf::Texture texture;
     texture.create(width, height);
+    Punto p1(100,200);
+    Punto p2(400,200);
+    Punto p3(400,400);
 
     std::cout << "Pintando pixeles" << std::endl;
-    pinta(300,300,0xFF0000FF);
-    pinta(500,300,0x00FF00FF);
-    pinta(300,500,0x00FFFF);
-    pinta(700,300,0xFF00FFFF);
+    //pinta(300,300,0xFF0000FF);
+    //pinta(500,300,0x00FF00FF);
+    //pinta(300,500,0x00FFFF);
+    //pinta(700,300,0xFF00FFFF);
     //circulo(400,300,50,blanco);
-    linea(100,400, rojo);
+    //linea(p1,p3, rojo);
+    cuadrado(p1,p2,amarillo);
 
 
 
--- punto.cpp
+++ punto.cpp
@@ -1,25 +1,26 @@
 class Punto{
     private:
-        int x;
-        int y;
-    
-    // Método contructor
-    Punto(int _x, int _y) : x(_x), y(_y){}
+        double x;
+        double y;
+      
+    public:
+        // Método contructor
+        Punto(double _x, double _y) : x(_x), y(_y){}
 
-    // Métodos de acceso a las coordenadas
-    int getX(){
-        return x;
-    }
+        // Métodos de acceso a las coordenadas
+       double getX(){
+            return x;
+        }  
 
-    int getY(){
-        return y;
-    }
+        double getY(){
+            return y;
+        }
 
-    void setX(int my_x){
-        x = my_x;
-    }
+        void setX(double my_x){
+            x = my_x;
+        }
 
-    void setY(int my_y){
-        y = my_y;
-    }
+        void setY(double my_y){
+            y = my_y;
+         }
 };
